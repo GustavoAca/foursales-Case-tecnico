43bdc412e86fa9f2f42cd189751ac103062efa6f
Gustavo Acácio <gustavo.acacio@attus.ai>
2025-02-23 15:58:36 -0300

    Ajustes finais

---
+++ README.md
@@ -0,0 +1,47 @@
+# Desafio Técnico - Desenvolvedor Pleno
+
+## Passo a passo para execução de
+
+## Dump banco de dados
+
+O arquivo se encontra na pasta `docker`
+
+## Execução de endpoints
+
+Decidi criar a estrutura pelo postman e compartilhar os endpoints, environments estão na pasta `enviroments_postman`
+
+## Modo de exportação
+
+Clique em importar
+![img.png](imagens/img-importar.png)
+
+Selecione arquivo
+![img.png](imagens/selecionar-arquivo.png)
+
+## Modo de execução do projeto
+
+### Docker
+
+Optando por utilizar o docker, basta entrar no arquivo `docker/docker-compose.yml` e executar o container da imagem do
+mysql com o comando abaixo:
+
+```bash
+docker compose up -d mysql 
+```
+
+Com isso, temos a conexão do banco disponivel, então iremos executar o projeto
+
+### Intelij
+
+<br/>
+
+Configuration
+
+![img.png](imagens/img-opcao-intelij.png)
+
+Criando application
+<br/>
+![img.png](imagens/criando-application.png)
+
+Configurando application e executando
+![img.png](img.png)
--- src/main/resources/db/migration/comum/V1_1_0_0__script_inicial.sql
+++ src/main/resources/db/migration/comum/V1_1_0_0__script_inicial.sql
@@ -1,4 +1,4 @@
-create table usuarios if not exists usuarios (
+create table IF NOT EXISTS usuarios (
   id binary(16) not null,
   email varchar(100) not null,
   senha varchar(100) not null,
@@ -13,10 +13,10 @@ create table usuarios if not exists usuarios (
 );
 
 alter table usuarios add constraint uq_usuario_email unique (email);
-create index idx_usuarios_total_de_compras_realizadas on usuarios(total_de_compras_realizadas);
-create index idx_usuarios_email on usuarios(email);
+CREATE INDEX IF NOT EXISTS idx_usuarios_total_de_compras_realizadas ON usuarios(total_de_compras_realizadas);
+create INDEX IF NOT EXISTS idx_usuarios_email on usuarios(email);
 
-create table categorias if not exists usuarios (
+create table IF NOT EXISTS categorias (
   id binary(16) not null,
   descricao varchar(250) not null,
   nome varchar(30) not null,
@@ -27,7 +27,7 @@ create table categorias if not exists usuarios (
   primary key (id)
 );
 
-create table subcategorias if not exists usuarios (
+create table IF NOT EXISTS subcategorias (
   id binary(16) not null,
   categoria_id binary(16) not null,
   descricao varchar(250) not null,
@@ -40,9 +40,9 @@ create table subcategorias if not exists usuarios (
   foreign key (categoria_id) references categorias(id)
 );
 
-create index idx_sucategorias_categoria_id on subcategorias(id);
+create INDEX IF NOT EXISTS idx_sucategorias_categoria_id on subcategorias(id);
 
-create table produtos if not exists usuarios (
+create table IF NOT EXISTS produtos (
   id binary(16) not null,
   subcategoria_id binary(16) not null,
   nome varchar(30) not null,
@@ -57,9 +57,9 @@ create table produtos if not exists usuarios (
   foreign key (subcategoria_id) references subcategorias(id)
 );
 
-create index idx_produtos_quantidade_em_estoque on produtos(quantidade_em_estoque);
+create INDEX IF NOT EXISTS idx_produtos_quantidade_em_estoque on produtos(quantidade_em_estoque);
 
-create table pedidos if not exists usuarios (
+create table IF NOT EXISTS pedidos (
   id binary(16) not null,
   usuario_id binary(16) not null,
   status varchar(30) not null,
@@ -72,11 +72,11 @@ create table pedidos if not exists usuarios (
   foreign key (usuario_id) references usuarios(id)
 );
 
-create index idx_pedidos_usuario_id on pedidos(usuario_id);
-create index idx_pedidos_usuario_id_valor_total on pedidos(usuario_id, valor_total);
-create index idx_pedidos_data_de_criacao_valor_total on pedidos(data_de_criacao, valor_total);
+create INDEX IF NOT EXISTS idx_pedidos_usuario_id on pedidos(usuario_id);
+create INDEX IF NOT EXISTS idx_pedidos_usuario_id_valor_total on pedidos(usuario_id, valor_total);
+create INDEX IF NOT EXISTS idx_pedidos_data_de_criacao_valor_total on pedidos(data_de_criacao, valor_total);
 
-create table pagamentos if not exists usuarios (
+create table IF NOT EXISTS pagamentos (
   id binary(16) not null,
   pedido_id binary(16) not null,
   data_de_criacao timestamp null default current_timestamp,
@@ -87,9 +87,9 @@ create table pagamentos if not exists usuarios (
   foreign key (pedido_id) references pedidos(id)
 );
 
-create index idx_pagamentos_pedido_id on pagamentos(pedido_id);
+create INDEX IF NOT EXISTS idx_pagamentos_pedido_id on pagamentos(pedido_id);
 
-create table produtos_pedidos if not exists usuarios (
+create table IF NOT EXISTS produtos_pedidos (
   id binary(16) not null,
   produto_id binary(16) not null,
   pedido_id binary(16) not null,
@@ -105,5 +105,5 @@ create table produtos_pedidos if not exists usuarios (
   foreign key (pedido_id) references pedidos(id)
 );
 
-create index idx_produtos_pedidos_pedido_id on produtos_pedidos(pedido_id);
-create index idx_produtos_pedidos_produto_id on produtos_pedidos(produto_id);
+create INDEX IF NOT EXISTS idx_produtos_pedidos_pedido_id on produtos_pedidos(pedido_id);
+create INDEX IF NOT EXISTS idx_produtos_pedidos_produto_id on produtos_pedidos(produto_id);
